6a6471d280783fc9b3657a2a0018b2d5dd400713
Fish <2140961727@qq.com>
2022-11-28 10:00:28 +0800

    IDEA third commit

--- src/main/java/GitHello.java
+++ src/main/java/GitHello.java
@@ -2,5 +2,6 @@ public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello,git~!");
         System.out.println("hello,git baby~!");
+        System.out.println("hello,git baby baby~!");
     }
 }
